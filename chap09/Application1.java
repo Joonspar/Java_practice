b2f721c9761c79f58d4bcf03a76c3c5806a85e5a
Joonspar <sdwang15@naver.com>
2025-02-24 17:22:39 +0900

    feat API 문제 해결

--- chap09/Application1.java
+++ chap09/Application1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Application1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("문자열 입력 : ");
+        System.out.print("문자열 입력: ");
         String s1 = sc.nextLine();
         String[] arr = s1.split(" ");
         int l = arr.length;
