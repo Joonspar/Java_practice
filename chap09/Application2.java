8ecedb25fb3cf0bce11268d73f1506071f2fc24b
Joonspar <sdwang15@naver.com>
2025-02-24 17:17:56 +0900

    feat 상속,다형성 해결

---
+++ chap09/Application2.java
@@ -0,0 +1,59 @@
+package joonseo.chap09;
+import java.util.Scanner;
+
+public class Application2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("문자열 입력 : ");
+        String input = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : input.toCharArray()) {
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == ' ') {
+                sb.append(Character.toLowerCase(ch));
+            }
+
+        }
+
+        // 3️⃣ 단어 분리
+        String[] words = sb.toString().split("\\s+");
+        String[] wordsarr = new String[words.length];
+        int[] counts = new int[words.length];
+        int ucnt = 0;
+
+        // 4️⃣ 단어 빈도 계산 (배열 활용)
+        for (String word : words) {
+            if (word.isEmpty()) continue;
+            boolean check = false;
+            for (int i = 0; i < ucnt; i++) {
+                if (wordsarr[i].equals(word)) {
+                    counts[i]++;
+                    check = true;
+                    break;
+                }
+            }
+
+            if (!check) {
+                wordsarr[ucnt] = word;
+                counts[ucnt] = 1;
+                ucnt++;
+            }
+        }
+
+        System.out.println("------ 출력 예시 ------");
+        System.out.println("------- 단어 빈도 -------");
+        int cnt = 0;
+        String result = "";
+
+        for (int i = 0; i < ucnt; i++) {
+            System.out.println(wordsarr[i] + ": " + counts[i]);
+
+            if (counts[i] > cnt) {
+                cnt = counts[i];
+                result = wordsarr[i];
+            }
+        }
+        System.out.println("가장 빈도 높은 단어 : " + result + " (" + cnt + " 번)");
+    }
+}
+
